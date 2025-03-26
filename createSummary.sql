42a628f3126b6f4008f74f8ed00359123c99d205
inigovilaa <ivila006@ikasle.ehu.eus>
2025-03-26 11:59:18 +0100

    conseguidoo

---
+++ createSummary.sql
@@ -0,0 +1,17 @@
+-- Crear la tabla externa summary en formato CSV en HDFS
+CREATE EXTERNAL TABLE IF NOT EXISTS summary (
+    country STRING,
+    numUsuarios BIGINT
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION 'user/hive/summary';  -- Ruta en HDFS
+
+-- Insertar datos en summary
+INSERT OVERWRITE TABLE summary
+SELECT country, COUNT(*) as numUsuarios
+FROM usuarios
+GROUP BY country
+ORDER BY numUsuarios DESC
+LIMIT 10;
